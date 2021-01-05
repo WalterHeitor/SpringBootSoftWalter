e2473ad2390dccb737111e3164949a81c097fd4a
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-04 22:19:38 -0300

    criar repositorio usuario

--- src/main/java/com/softwalter/project/douman/Request.java
+++ src/main/java/com/softwalter/project/douman/Request.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import com.softwalter.project.douman.enums.RequestStates;
 
 import lombok.AllArgsConstructor;
@@ -13,15 +26,31 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Request {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String subject;
+	@Column(columnDefinition = "text")
 	private String description;
+	@Column(name = "creation_date")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date creationDate;
+	@Column
+	@Enumerated(EnumType.STRING)
 	private RequestStates requestStates;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
 	private Users users;
-	private List<RequestStates>states = new ArrayList<RequestStates>();
+	
+	@OneToMany(mappedBy = "request")
+	private List<RequestStages>states = new ArrayList<>();
+	
+	
 	public Request(Long id, String subject, String description,
 				Date creationDate, RequestStates requestStates,
 				Users users) {
--- src/main/java/com/softwalter/project/douman/Users.java
+++ src/main/java/com/softwalter/project/douman/Users.java
@@ -3,19 +3,45 @@ package com.softwalter.project.douman;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.softwalter.project.douman.enums.RequestStates;
+import com.softwalter.project.douman.enums.Role;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Entity(name = "user")
 public class Users {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String nome;
+	@Column
 	private String email;
+	@Column
 	private String password;
+	
+	@Column
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	@OneToMany(mappedBy = "users")
 	private List<Request>requests = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "users")
+	private List<RequestStates>states = new ArrayList<>();
 	
 }
