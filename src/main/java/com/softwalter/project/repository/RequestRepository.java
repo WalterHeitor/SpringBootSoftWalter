452c14776bb8c7750c4182af8edae07923c06ac0
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-07 00:27:39 -0300

    RequestStages

--- src/main/java/com/softwalter/project/repository/RequestRepository.java
+++ src/main/java/com/softwalter/project/repository/RequestRepository.java
@@ -2,6 +2,8 @@ package com.softwalter.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RequestRepository extends JpaRepository<T, ID>{
+import com.softwalter.project.douman.Request;
+
+public interface RequestRepository extends JpaRepository<Request, Long>{
 
 }
